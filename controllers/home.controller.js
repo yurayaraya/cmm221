2e22c86d767b5d6303c91d3373215e092be189c1
yurayaraya <32543157+yurayaraya@users.noreply.github.com>
2017-10-07 16:13:21 +0700

    Add content to twig

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,3 +1,29 @@
 exports.index = function(req, res){
-	res.render('home.twig')
+
+	let content ={
+		highlight: {
+			url: '/assets/images/home/banner/highlight.jpg',
+			title: 'Aunty\'s Garden'
+		},
+		thumbnail: [
+			{
+				url: '/assets/images/home/banner/thumbnail-1.jpg',
+				title: 'thumbnail 1'
+			},
+			{
+				url: '/assets/images/home/banner/thumbnail-2.jpg',
+				title: 'thumbnail 2'
+			},
+			{
+				url: '/assets/images/home/banner/thumbnail-3.jpg',
+				title: 'thumbnail 3'
+			},
+			{
+				url: '/assets/images/home/banner/thumbnail-4.jpg',
+				title: 'thumbnail 4'
+			}
+		]
+	}
+
+	res.render('home.twig', content)
 }
--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -1,18 +1,3 @@
 exports.index = function(req, res){
-	let content = {
-		news: [ 
-			{
-				id: 1,
-				url: '//image/img-1.jpg',
-				title: 'news 1'
-			},
-			{
-				id: 2,
-				url: '//image/img-1.jpg',
-				title: 'news 2'
-			}
-		]
-	}
-
-	res.json(content)
+	res.render('news.twig')
 }
